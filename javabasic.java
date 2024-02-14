9c176b0bd1c4f22f731f0b97da1ed64d29b99a2c
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 01:02:24 +0530

    Add files via upload

---
+++ javabasic.java
@@ -0,0 +1,7 @@
+public class javabasic {
+    public static void main(String args[]) {
+        System.out.print("hello world\n");//line space
+        System.out.println("hello world");//line space
+        System.out.println("Hello world");//line space
+    }
+}// boilerplate code(a code in which we write every time in the program)
