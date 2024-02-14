e3dfb43d018d2c2d635031ac28439817db439ac5
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 01:03:46 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class practice{
+    public static void main(String args[])
+    {/* 1)
+        //average of two number
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int average=(a+b+c)/3;
+        System.out.println("the average of three numbers:"+average);
+        */
+        /* 
+        2)area of square
+        Scanner sc=new Scanner(System.in);
+        int side=sc.nextInt();
+        int area=side*side;
+        System.out.println(area);
+        */
+        /* 
+        //total cost of items
+        Scanner sc=new Scanner(System.in);
+        float pen=sc.nextFloat();
+        float pencil=sc.nextFloat();
+        float eraser=sc.nextFloat();
+        float total=pen+pencil+eraser;
+        System.out.println("total cost of the item "+total);
+*/
+
+        
+
+    }
+}
