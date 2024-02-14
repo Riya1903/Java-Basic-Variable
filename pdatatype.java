6fe5f6db481ec4b991e8692bd29036f9d44448f5
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 01:03:26 +0530

    Add files via upload

---
+++ pdatatype.java
@@ -0,0 +1,18 @@
+public class pdatatype{
+    public static void main(String args[]){
+        byte b=8;
+        System.out.println(b);
+        char ch='a';//print single character type
+        System.out.println(ch);
+        boolean var=true;
+        System.out.println(var);
+        float f=2.4f;
+        System.out.println(f);
+        int number =25;
+        System.out.println(number);
+        //long
+        //double
+        short n=240;
+        System.out.println(n);
+  }
+}
