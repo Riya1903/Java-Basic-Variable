19a03d87af36bf0405aadea10b4a06590cbd11f1
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 01:06:30 +0530

    Add files via upload

---
+++ typepromotion.java
@@ -0,0 +1,23 @@
+public class typepromotion{
+    public static void main(String args[]){
+       /*  char a='a';
+        char b='b';
+        short x=23;
+        byte y=32;
+        char ch='c';
+        byte bt=(byte)(x+y+ch);
+        System.out.println(bt);
+     */
+       // System.out.println((int)(a));
+        //System.out.println((int)(b));
+        //System.out.println(b-a);
+        int i=2;
+        float j=4.6f;
+        long k=43;
+        double d=30;
+        double ans=i+ j+ k+ d;
+        System.out.println(ans);
+
+
+    }
+}
