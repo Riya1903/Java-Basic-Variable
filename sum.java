968e9b834715e6c8b6a21b833c9c998d504a4b3e
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 01:05:36 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum{
+    public static void main(String args[])
+    {
+        //code to calculate sum(single line comment)
+        /* code to calculate sum(multiline coment)*/
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}
